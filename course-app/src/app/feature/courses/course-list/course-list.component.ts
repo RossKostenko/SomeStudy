7fbfe19ad4b4edb5d94b760902d1a70428d3f401
Rostyslav Kostenko <rostyslav.kostenko@starz.com>
2022-04-18 21:07:02 +0300

    course-5-routing: prog routing, icon fix

--- course-app/src/app/feature/courses/course-list/course-list.component.ts
+++ course-app/src/app/feature/courses/course-list/course-list.component.ts
@@ -1,16 +1,11 @@
-import {
-  Component,
-  ChangeDetectionStrategy,
-  Input,
-  Output,
-  EventEmitter,
-} from "@angular/core";
-import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
-import { CourseItem } from "src/app/feature/courses/course-card/course-item.model";
+import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { CourseItem } from 'src/app/feature/courses/course-card/course-item.model';
 
 @Component({
-  selector: "app-course-list",
-  templateUrl: "./course-list.component.html",
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseListComponent {
@@ -21,14 +16,19 @@ export class CourseListComponent {
   @Output() courseEditOutput = new EventEmitter();
   @Output() courseDeleteOutput = new EventEmitter();
 
-  public editIcon = faPen;
   public deleteIcon = faTrash;
-  public infoText =
-    "Please use 'Add new course' button to add your first course";
-  public infoTitle = "Your List Is Empty";
+  public editIcon = faPen;
 
-  onShowCourse() {
-    this.courseClickOutput.emit();
+  public isAllCoursesShown = true;
+
+  public infoText = "Please use 'Add new course' button to add your first course";
+  public infoTitle = 'Your List Is Empty';
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  onShowCourse(course: CourseItem) {
+    this.isAllCoursesShown = false;
+    this.router.navigate([`${course.id}`], { relativeTo: this.activatedRoute });
   }
 
   onCourseEdit() {
