d52e9959b8761a149367265af71f6d5868abb689
Rostyslav Kostenko <rostyslav.kostenko@starz.com>
2022-04-18 21:05:29 +0300

    Added basic routing config

--- course-app/src/app/app-routing.module.ts
+++ course-app/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseCardEditComponent } from './feature/courses/course-card-edit/course-card-edit.component';
+import { CourseCardComponent } from './feature/courses/course-card/course-card.component';
+import { CoursesComponent } from './feature/courses/courses.component';
+import { LoginComponent } from './feature/login/login.component';
+import { RegistrationComponent } from './feature/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/courses', pathMatch: 'full' },
+  {
+    path: 'courses',
+    component: CoursesComponent,
+    children: [
+      { path: 'add', component: CourseCardEditComponent },
+      { path: ':id', component: CourseCardComponent },
+      { path: ':id/edit', component: CourseCardEditComponent },
+    ],
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
